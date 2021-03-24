a27ebed608006dd024d70f0586242484bbddbe12
Una LEstrange <lestrange.una@itsligo.ie>
2021-03-24 10:11:53 +0000

    added employee class and did basic worksheet

---
+++ Employee/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    class Employee
+    {
+
+        public static int lastEmployeeNumber;
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int EmployeeNumber { get; set; }
+        public decimal HourlyRate { get; set; }
+
+        private string nationality;
+
+        public string Nationality
+        {
+            get { return nationality; }
+            set
+            {
+                if (value == "British")
+                { nationality = "Visa Required"; }
+                else
+                { nationality = value; }
+            }
+        }
+
+
+        //default constructor
+        public Employee()
+        {
+            lastEmployeeNumber++;
+            EmployeeNumber = lastEmployeeNumber;
+        }
+        //parameterised constructor
+        public Employee(string name, string gen, decimal payRate, string nat)
+        {
+            Name = name;
+            Gender = gen;
+            HourlyRate = payRate;
+            lastEmployeeNumber++;
+            EmployeeNumber = lastEmployeeNumber;
+            Nationality = nat;
+        }
+        //method
+        public override string ToString()
+        {
+            return String.Format("Name is {0}, Gender is {1}, Rate is {2}, " +
+                 "Number is {3}, Nationality is {4}",
+                 Name, Gender, HourlyRate, EmployeeNumber, Nationality); ;
+
+        }
+
+        public virtual decimal CalcPay()
+        {
+            return 40 * HourlyRate;
+        }
+
+        public virtual decimal CalcTax()
+        {
+            return (CalcPay() * .4m) ;
+        }
+    }
+
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmp = new Employee();
+
+            myEmp.Name = "Una L'Estrange";
+            myEmp.Gender = "Female";
+            myEmp.HourlyRate = 500;
+
+            Console.WriteLine(myEmp.ToString());
+
+            Employee secondEmp = new Employee("Viv", "Male", 10, "British");
+
+            Console.WriteLine(secondEmp.ToString());
+
+            Console.WriteLine("Tax is {0}", myEmp.CalcTax());
+            Console.WriteLine("Tax is {0}", secondEmp.CalcTax());
+        }
+    }
+}
