712996a5c577d3d1da417e6fc7ed9af0057a8f8f
Una LEstrange <lestrange.una@itsligo.ie>
2021-03-24 10:54:39 +0000

    added part time employee

---
+++ Employee/PartTimeEmployee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    class PartTimeEmployee : Employee
+    {
+        public int HoursWorked { get; set; }
+
+
+        public PartTimeEmployee() : base()
+        {
+        }
+
+        public PartTimeEmployee(string name, string gen, decimal payRate, string nat,
+            int hrsWorked) : base (name, gen, payRate, nat)
+        {
+            HoursWorked = hrsWorked;
+        }
+
+        public override decimal CalcPay()
+        {
+            return (HoursWorked * HourlyRate);
+        }
+
+        public override string ToString()
+        {
+           return ( base.ToString() + ", Hours Worked " + HoursWorked);
+        }
+    }
+
+
+}
--- Employee/Program.cs
+++ Employee/Program.cs
@@ -6,6 +6,10 @@ namespace Employee
     {
         static void Main(string[] args)
         {
+
+            Employee[] myEmpire = new Employee[5];
+
+
             Employee myEmp = new Employee();
 
             myEmp.Name = "Una L'Estrange";
@@ -20,6 +24,24 @@ namespace Employee
 
             Console.WriteLine("Tax is {0}", myEmp.CalcTax());
             Console.WriteLine("Tax is {0}", secondEmp.CalcTax());
+
+            myEmpire[0] = myEmp;
+            myEmpire[1] = secondEmp;
+            myEmpire[2] = new PartTimeEmployee("Kevin", "Male", 30, "French", 10);
+            myEmpire[3] = new Employee("Mary Loftus", "Female", 20, "Irish");
+            myEmpire[4] = new PartTimeEmployee("Drew", "Male", 30, "Irish", 20);
+
+            secondEmp.Name = "Vivian";
+            myEmpire[1].HourlyRate = 1000;
+
+            for (int i = 0; i < myEmpire.Length; i++)
+            {
+                string name = myEmpire[i].Name;
+                decimal pay = myEmpire[i].CalcPay();
+
+
+                Console.WriteLine("{0} is owed {1}", name, pay);
+            }
         }
     }
 }
