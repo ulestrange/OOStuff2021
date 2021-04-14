a34c55d9ac81b29cb0ce42a24cffd7f90870347f
Una LEstrange <lestrange.una@itsligo.ie>
2021-04-14 14:02:41 +0100

    changed for demo

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -7,41 +7,46 @@ namespace Employee
         static void Main(string[] args)
         {
 
-            Employee[] myEmpire = new Employee[5];
+            PartTimeEmployee emp1 = new PartTimeEmployee();
+
+            Console.WriteLine(emp1.ToString());
+
+            //    Employee[] myEmpire = new Employee[5];
 
 
-            Employee myEmp = new Employee();
+            //    Employee myEmp = new Employee();
 
-            myEmp.Name = "Una L'Estrange";
-            myEmp.Gender = "Female";
-            myEmp.HourlyRate = 500;
+            //    myEmp.Name = "Una L'Estrange";
+            //    myEmp.Gender = "Female";
+            //    myEmp.HourlyRate = 500;
 
-            Console.WriteLine(myEmp.ToString());
+            //    Console.WriteLine(myEmp.ToString());
 
-            Employee secondEmp = new Employee("Viv", "Male", 10, "British");
+            //    Employee secondEmp = new Employee("Viv", "Male", 10, "British");
 
-            Console.WriteLine(secondEmp.ToString());
+            //    Console.WriteLine(secondEmp.ToString());
 
-            Console.WriteLine("Tax is {0}", myEmp.CalcTax());
-            Console.WriteLine("Tax is {0}", secondEmp.CalcTax());
+            //    Console.WriteLine("Tax is {0}", myEmp.CalcTax());
+            //    Console.WriteLine("Tax is {0}", secondEmp.CalcTax());
 
-            myEmpire[0] = myEmp;
-            myEmpire[1] = secondEmp;
-            myEmpire[2] = new PartTimeEmployee("Kevin", "Male", 30, "French", 10);
-            myEmpire[3] = new Employee("Mary Loftus", "Female", 20, "Irish");
-            myEmpire[4] = new PartTimeEmployee("Drew", "Male", 30, "Irish", 20);
+            //    myEmpire[0] = myEmp;
+            //    myEmpire[1] = secondEmp;
+            //    myEmpire[2] = new PartTimeEmployee("Kevin", "Male", 30, "French", 10);
+            //    myEmpire[3] = new Employee("Mary Loftus", "Female", 20, "Irish");
+            //    myEmpire[4] = new PartTimeEmployee("Drew", "Male", 30, "Irish", 20);
 
-            secondEmp.Name = "Vivian";
-            myEmpire[1].HourlyRate = 1000;
+            //    secondEmp.Name = "Vivian";
+            //    myEmpire[1].HourlyRate = 1000;
 
-            for (int i = 0; i < myEmpire.Length; i++)
-            {
-                string name = myEmpire[i].Name;
-                decimal pay = myEmpire[i].CalcPay();
+            //    for (int i = 0; i < myEmpire.Length; i++)
+            //    {
+            //        string name = myEmpire[i].Name;
+            //        decimal pay = myEmpire[i].CalcPay();
 
 
-                Console.WriteLine("{0} is owed {1}", name, pay);
-            }
+            //        Console.WriteLine("{0} is owed {1}", name, pay);
+            //    }
+            //}
         }
     }
 }
---
+++ Lab8/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateTempReport();
+        }
+
+        static void WriteSongLine ()
+        {
+
+            string filename1 = "../../../song2.txt";
+            string filename2 = "song2.txt";
+            string filename3 = @"C:\Users\lestr\Desktop\song4.csv";
+            string filename4 = "C:\\Users\\lestr\\Desktop\\song5.txt";
+
+            FileStream fs = new FileStream(filename4, FileMode.Create, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine("Hello is it me you're lookin for");
+
+            sw.Close();
+        }
+
+        static void CreateTempReport ()
+
+        {
+            string fileToRead = "../../../temp.txt";
+
+            string fileToWrite = "../../../report.txt";
+
+            string lineIn; // will hold data that we read in
+
+            FileStream fsIn = new FileStream(fileToRead, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fsIn);
+
+            FileStream fsOut = new FileStream(fileToWrite, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fsOut);
+
+            sw.WriteLine("{0,10}{1,15}", "Date", "Temperature");
+
+            lineIn = sr.ReadLine(); // read in first line from file
+
+            sw.WriteLine(FormatLineForReport(lineIn));
+
+            while (lineIn != null) // null signals we are end of the file
+            {
+                Console.WriteLine(lineIn); // print to screen
+                sw.WriteLine(FormatLineForReport(lineIn));
+
+                lineIn = sr.ReadLine(); // read in next line from file
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+        
+        static string FormatLineForReport (string lineIn)
+        {
+            string[] fields = new string[2];
+            fields = lineIn.Split(',');
+
+            return String.Format("{0,10}{1,15}", fields[0], fields[1]);
+
+        }
+    }
+}
